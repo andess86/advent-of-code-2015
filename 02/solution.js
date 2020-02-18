420d6799b91098395f71651f29f533286ee6b414
Anders Svensson <andess86@gmail.com>
2020-02-18 18:31:57 +0100

    Add solution for day 2, first part

---
+++ 02/solution.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt', 'utf-8', (input, err) => {
+  if (err) {
+    console.log(err);
+  }
+});
+
+const arrayOfBoxes = input.split('\n');
+let arrayOfBoxesDimensions = [];
+
+for (let i = 0; i < arrayOfBoxes.length; i++) {
+  arrayOfBoxesDimensions.push(arrayOfBoxes[i].split('x'));
+}
+let totalInchesIfPresentPaper = 0;
+for (let i = 0; i < arrayOfBoxesDimensions.length; i++) {
+  const val1 = 2 * (parseInt(arrayOfBoxesDimensions[i][0]) * parseInt(arrayOfBoxesDimensions[i][1]));
+  const val2 = 2 * (parseInt(arrayOfBoxesDimensions[i][1]) * parseInt(arrayOfBoxesDimensions[i][2]));
+  const val3 = 2 * (parseInt(arrayOfBoxesDimensions[i][2]) * parseInt(arrayOfBoxesDimensions[i][0]));
+  const totalInchesForThisBox = val1 + val2 + val3 + (Math.min(val1, val2, val3) / 2);
+  totalInchesIfPresentPaper = totalInchesIfPresentPaper + totalInchesForThisBox;
+}
+
+console.log(`I suggest the elves order a total of ${totalInchesIfPresentPaper} square feet of wrapping paper.`);
