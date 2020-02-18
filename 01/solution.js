3f9b7086182a6115f06ebc7ef4a2a4aada62ecc4
Anders Svensson <andess86@gmail.com>
2020-02-18 17:35:16 +0100

    Add solution for day 1, part 2

--- 01/solution.js
+++ 01/solution.js
@@ -5,9 +5,18 @@ const text = fs.readFileSync('input.txt', 'utf-8', (text, err) => {
     console.log(err);
   }
 });
+
 const arrayOfSteps = text.split('');
 let floor = 0;
+let reachedBasement = 'false';
+
 for (let i = 0; i < arrayOfSteps.length; i++) {
   arrayOfSteps[i] === '(' ? (floor = floor + 1) : (floor = floor - 1);
+  if (floor === -1 && reachedBasement === 'false') {
+    reachedBasement = 'true';
+    console.log(
+      `The position of the instructions when Santa first enters the basement is ${i + 1}`
+    );
+  }
 }
 console.log(`The instructions take Santa to the ${floor}th floor.`);
