132561b8423c6c29ffb629ed68c1f86d1b60cf49
Anders Svensson <andess86@gmail.com>
2020-02-19 16:26:42 +0100

    Add solution for day 3, part 2

--- 03/solution.js
+++ 03/solution.js
@@ -8,35 +8,68 @@ const input = fs.readFileSync('input.txt', 'utf-8', (input, err) => {
 const instructions = input.split('');
 
 // Didn't use following function in the end, got the same result using [... new Set(array)]
-
 // const onlyUnique = (value, index, self) => {
 //   return self.indexOf(value) === index;
 // }
 
-const directionsArray = [];
-let xAxis = 0;
-let yAxis = 0;
-for (let i = 1; i < instructions.length; i++) {
-  switch (instructions[i]) {
-    case '<':
-      xAxis = xAxis - 1;
-      directionsArray.push(`${xAxis} ${yAxis}`);
-      break;
-    case '>':
-      xAxis = xAxis + 1;
-      directionsArray.push(`${xAxis} ${yAxis}`);
-      break;
-    case 'v':
-      yAxis = yAxis - 1;
-      directionsArray.push(`${xAxis} ${yAxis}`);
-      break;
-    case '^':
-      yAxis = yAxis + 1;
-      directionsArray.push(`${xAxis} ${yAxis}`);
-      break;
-    default:
-      break;
+const santaDirectionsArray = [];
+const roboSantaDirectionsArray = [];
+let santaXAxis = 0;
+let santaYAxis = 0;
+let roboSantaXAxis = 0;
+let roboSantaYAxis = 0;
+
+const isEven = instruction => {
+  return instruction % 2 == 0;
+};
+
+for (let i = 0; i < instructions.length; i++) {
+  if (isEven(i)) {
+    switch (instructions[i]) {
+      case '<':
+        santaXAxis = santaXAxis - 1;
+        santaDirectionsArray.push(`${santaXAxis} ${santaYAxis}`);
+        break;
+      case '>':
+        santaXAxis = santaXAxis + 1;
+        santaDirectionsArray.push(`${santaXAxis} ${santaYAxis}`);
+        break;
+      case 'v':
+        santaYAxis = santaYAxis - 1;
+        santaDirectionsArray.push(`${santaXAxis} ${santaYAxis}`);
+        break;
+      case '^':
+        santaYAxis = santaYAxis + 1;
+        santaDirectionsArray.push(`${santaXAxis} ${santaYAxis}`);
+        break;
+      default:
+        break;
+    }
+  } else {
+    switch (instructions[i]) {
+      case '<':
+        roboSantaXAxis = roboSantaXAxis - 1;
+        roboSantaDirectionsArray.push(`${roboSantaXAxis} ${roboSantaYAxis}`);
+        break;
+      case '>':
+        roboSantaXAxis = roboSantaXAxis + 1;
+        roboSantaDirectionsArray.push(`${roboSantaXAxis} ${roboSantaYAxis}`);
+        break;
+      case 'v':
+        roboSantaYAxis = roboSantaYAxis - 1;
+        roboSantaDirectionsArray.push(`${roboSantaXAxis} ${roboSantaYAxis}`);
+        break;
+      case '^':
+        roboSantaYAxis = roboSantaYAxis + 1;
+        roboSantaDirectionsArray.push(`${roboSantaXAxis} ${roboSantaYAxis}`);
+        break;
+      default:
+        break;
+    }
   }
 }
-var uniqueAddresses = [...new Set(directionsArray)];
-console.log(`${uniqueAddresses.length} houses recieved at least one present.`);
+const combinedArray = santaDirectionsArray.concat(roboSantaDirectionsArray);
+var uniqueAddresses = [...new Set(santaDirectionsArray)];
+var uniqueAddressesSecondYear = [...new Set(combinedArray)];
+console.log(`${uniqueAddresses.length} houses recieved at least one present from Santa.`);
+console.log(`${uniqueAddressesSecondYear.length} houses recieved at least one present from Santa with the help of RoboSanta on year 2!`);
