5e543c19b3446b8fd0803d65db54154bc9a8a124
Anders Svensson <andess86@gmail.com>
2020-02-19 17:04:23 +0100

    Add solution for day 4, part 1

---
+++ 04/solution.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+const secretKey = fs.readFileSync('input.txt', 'utf-8', (input, err) => {
+  if (err) {
+    console.log(err);
+  }
+});
+
+let iterator = 0;
+let foundSolution = false;
+
+while (!foundSolution) {
+  let hash = crypto
+    .createHash('md5')
+    .update(secretKey + iterator)
+    .digest('hex');
+
+  if (hash.substring(0, 5) === '00000') {
+    foundSolution = true;
+  }
+  iterator = iterator + 1;
+}
+
+console.log(`The lowest positive number that, combined with the secret key, produces such a hash is: ${iterator}.`);
