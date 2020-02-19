8a23fa18c780164d0939111788b7ea696a500f9b
Anders Svensson <andess86@gmail.com>
2020-02-19 12:22:17 +0100

    Fix typo

--- 02/solution.js
+++ 02/solution.js
@@ -12,13 +12,13 @@ let arrayOfBoxesDimensions = [];
 for (let i = 0; i < arrayOfBoxes.length; i++) {
   arrayOfBoxesDimensions.push(arrayOfBoxes[i].split('x'));
 }
-let totalInchesIfPresentPaper = 0;
+let totalInchesOfPresentPaper = 0;
 for (let i = 0; i < arrayOfBoxesDimensions.length; i++) {
   const val1 = 2 * (parseInt(arrayOfBoxesDimensions[i][0]) * parseInt(arrayOfBoxesDimensions[i][1]));
   const val2 = 2 * (parseInt(arrayOfBoxesDimensions[i][1]) * parseInt(arrayOfBoxesDimensions[i][2]));
   const val3 = 2 * (parseInt(arrayOfBoxesDimensions[i][2]) * parseInt(arrayOfBoxesDimensions[i][0]));
   const totalInchesForThisBox = val1 + val2 + val3 + (Math.min(val1, val2, val3) / 2);
-  totalInchesIfPresentPaper = totalInchesIfPresentPaper + totalInchesForThisBox;
+  totalInchesOfPresentPaper = totalInchesOfPresentPaper + totalInchesForThisBox;
 }
 
 console.log(`I suggest the elves order a total of ${totalInchesIfPresentPaper} square feet of wrapping paper.`);
