e6f773c6a6d41f0953375d9ab60f4e4dd7c3d198
Anders Svensson <andess86@gmail.com>
2020-02-22 13:46:44 +0100

    Solution for day 6, part 1

--- 06/solution.js
+++ 06/solution.js
@@ -9,13 +9,18 @@ const input = fs.readFileSync('input.txt', 'utf-8', (input, err) => {
 const instructions = input.split('\n');
 const cmdArray = [];
 
-const getInstructions = text => {
-  if (text.substring(0, 7) === 'turn on') {
-    return text.substring(0, 7);
-  } else if (text.substring(0, 8) === 'turn off') {
-    return text.substring(0, 8);
+var lightGrid = [];
+for (var i = 0; i < 1000; i++) {
+  lightGrid[i] = new Array(1000).fill(false);
+}
+
+const getInstructions = instruction => {
+  if (instruction.substring(0, 7) === 'turn on') {
+    return instruction.substring(0, 7);
+  } else if (instruction.substring(0, 8) === 'turn off') {
+    return instruction.substring(0, 8);
   } else {
-    return text.substring(0, 6);
+    return instruction.substring(0, 6);
   }
 };
 
@@ -34,4 +39,54 @@ for (let i = 0; i < instructions.length; i++) {
   ]);
 }
 
-console.table(cmdArray);
+const turnOn = (l, t, r, b) => {
+  console.log(`Turning on ${l},${t} through ${r},${b}.`);
+  for (let x = l; x <= r; x++) {
+    for (let y = t; y <= b; y++) {
+      lightGrid[x][y] = true;
+    }
+  }
+};
+
+const turnOff = (l, t, r, b) => {
+  console.log(`Turning off ${l},${t} through ${r},${b}.`);
+  for (let x = l; x <= r; x++) {
+    for (let y = t; y <= b; y++) {
+      lightGrid[x][y] = false;
+    }
+  }
+};
+
+const toggle = (l, t, r, b) => {
+  console.log(`Switching state on ${l},${t} through ${r},${b}.`);
+  for (let x = l; x <= r; x++) {
+    for (let y = t; y <= b; y++) {
+      lightGrid[x][y] = !lightGrid[x][y];
+    }
+  }
+};
+
+cmdArray.map(cmd => {
+  switch (cmd[0]) {
+    case 'turn on':
+      turnOn(cmd[1], cmd[2], cmd[3], cmd[4]);
+      break;
+    case 'turn off':
+      turnOff(cmd[1], cmd[2], cmd[3], cmd[4]);
+      break;
+    case 'toggle':
+      toggle(cmd[1], cmd[2], cmd[3], cmd[4]);
+      break;
+
+    default:
+      break;
+  }
+});
+
+// Solution, part 1
+let lightsOn = 0;
+lightGrid.map(lightRow => {
+  lightsOn = lightsOn + lightRow.filter(Boolean).length;
+});
+
+console.log(`\nAfter following the instructions, ${lightsOn} are on.`);
